e309ad95a3b1bec0a347ba43072dd7b09d61c4aa
HutEight <sxh759@case.edu>
2018-04-16 13:09:22 -0400

    fixed small bugs

--- 20180112_new_calibration_MK9/generate_frozen_grid_playfile_main_CUBE.m
+++ 20180112_new_calibration_MK9/generate_frozen_grid_playfile_main_CUBE.m
@@ -15,11 +15,11 @@ z = -0.1;
 time = 10;
 count = 1;
 
-for i = 0:9
+for i = 0:5
     
-   for j = 0:9
+   for j = 0:5
        
-       for k = 0:9
+       for k = 0:5
 
           disp(strcat(num2str((x+i*0.01)), ',  ',num2str(y+j*0.01), ',  ' ,num2str(z+k*0.01), ',0,1,0, 0, 0, -1, -1,       0, 0, -0.15, 0,1,0, 0, 0, -1, 0,',   num2str(time)));
           disp(strcat(num2str((x+i*0.01)), ',  ',num2str(y+j*0.01), ',  ' ,num2str(z+k*0.01), ',0,1,0, 0, 0, -1, -1,       0, 0, -0.15, 0,1,0, 0, 0, -1, 0,',   num2str(time + 2))); 
