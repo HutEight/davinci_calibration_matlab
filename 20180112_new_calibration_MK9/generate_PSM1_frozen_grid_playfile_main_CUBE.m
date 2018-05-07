841e7248a705078d54ea8d0110f983ad6f984c7d
HutEight <sxh759@case.edu>
2018-05-07 12:48:18 -0400

    dual_cubes

---
+++ 20180112_new_calibration_MK9/generate_PSM1_frozen_grid_playfile_main_CUBE.m
@@ -0,0 +1,52 @@
+% RN@HMS Prince of Wales
+% 07/05/18
+
+%%
+clc
+close all
+clear all
+
+%% 
+% Change these initial values
+% centre of the 7x7 cube
+centre_x = -0.1;
+centre_y = 0.15;
+centre_z = -0.15;
+
+psm_x = [1 0 0];
+
+axis_vec = [centre_x centre_y centre_z];
+cube_z = axis_vec/norm(axis_vec);
+cube_x = cross(psm_x, cube_z)/norm(cross(psm_x, cube_z));
+cube_y = cross(cube_z, cube_x)/norm(cross(cube_z, cube_x));
+
+rot_cube_wrt_psm = transpose([cube_x; cube_y; cube_z]);
+
+corner = [centre_x centre_y centre_z] - 0.04*(cube_x + cube_y + cube_z);
+
+time = 10;
+count = 1;
+
+for i = 0:6
+    
+   for j = 0:6
+       
+       for k = 0:6
+           
+          point = corner + i*0.01*cube_x + j*0.01*cube_y + k*0.01*cube_z;
+
+          disp( strcat(num2str(point(1)), ',  ', num2str(point(2)), ',  ' , num2str(point(3)), ',0,1,0, 0, 0, -1, -1,       0, 0, -0.15, 0,1,0, 0, 0, -1, 0,',   num2str(time)));
+          disp( strcat(num2str(point(1)), ',  ', num2str(point(2)), ',  ' , num2str(point(3)), ',0,1,0, 0, 0, -1, -1,       0, 0, -0.15, 0,1,0, 0, 0, -1, 0,',   num2str(time + 2))); 
+
+          time = time + 4;
+
+          psm1_pts_generated_cube(count,:) = [point(1) point(2) point(3)];
+          count = count + 1;
+      
+       end
+   end
+    
+end
+
+
+save('psm1_pts_generated_cube.mat', 'psm1_pts_generated_cube');
---
+++ 20180112_new_calibration_MK9/process_PSM1_grid_data_CUBE.m
@@ -0,0 +1,111 @@
+% RN@HMS Queen Elizabeth
+% 16/04/18
+
+%%
+clc
+close all
+clear all
+
+%%
+% fill in the blank
+file_path = '20180416_frozen_data_01/green_frozen.csv'
+csv = csvread(file_path);
+
+% check if they are correctly numbered.
+seq = csv(:, 6);
+% should make it represent this much seconds
+seq = ((seq - seq(1)) / 1);
+raw_pose_x = csv(:, 3);
+raw_pose_y = csv(:, 4);
+raw_pose_z = csv(:, 5);
+
+raw_points = [seq, raw_pose_x, raw_pose_y, raw_pose_z];
+raw_size = size(raw_points,1);
+
+figure('Name','Polaris Points');
+axis equal;
+scatter3(raw_points(:,2), raw_points(:,3), raw_points(:,4), 'filled');
+hold off;
+
+%% 
+% fill in start time (second) Needs adjustment each time
+time_0 = 12.5; % Use 12.5 for some reason... Not 10
+time_t = time_0 + 0.5; % take 1 sec of samples equvilent of 20 pts
+peroid = 4; % 2 + 2 seconds
+
+% Size of data: from a cube of 5x5x5
+length = 6;
+n_pts = length*length*length; 
+
+for i = 0:(n_pts-1)
+    
+   mask_begin = time_0 + i*peroid;
+   mask_end = time_t + i*peroid;
+   mask = (raw_points(:,1) > mask_begin & raw_points(:,1) < mask_end);
+   
+   pt_mat_0 = [seq(mask), raw_pose_x(mask), raw_pose_y(mask), raw_pose_z(mask)];
+   pt_mat_0(isnan(pt_mat_0(:,2)),:)= [];
+   
+   pt_mat = [pt_mat_0(:,2), pt_mat_0(:,3), pt_mat_0(:,4)];
+   
+   x_ave = mean(pt_mat_0(:,2));
+   y_ave = mean(pt_mat_0(:,3));
+   z_ave = mean(pt_mat_0(:,4));
+   
+   psm1_pts_Polaris_cube(i+1,:) = [x_ave y_ave z_ave];
+    
+    
+end
+
+save('psm1_pts_Polaris_cube.mat', 'psm1_pts_Polaris_cube');
+
+
+%% Fit planes and plot
+figure('Name','Polaris Points Plane Fitted');
+axis equal;
+hold on;
+for i=1:length
+    
+    pm = plane(psm1_pts_Polaris_cube((length^0 + (i-1)*(length^2)):(length^2 + (i-1)*(length^2)),1), ...
+               psm1_pts_Polaris_cube((length^0 + (i-1)*(length^2)):(length^2 + (i-1)*(length^2)),2), ...
+               psm1_pts_Polaris_cube((length^0 + (i-1)*(length^2)):(length^2 + (i-1)*(length^2)),3)); 
+    
+    a = pm.Parameters(1);
+    b = pm.Parameters(2);
+    c = pm.Parameters(3);
+    d = pm.Parameters(4);
+    
+    % RMS
+    for n = 1:(length^2)
+
+        dist_square = ((a*psm1_pts_Polaris_cube(n,1)+b*psm1_pts_Polaris_cube(n,2)+c*psm1_pts_Polaris_cube(n,3)+d)^2)/(a^2 + b^2 + c^2);
+        
+    end
+    
+    rms(i) = sqrt(dist_square/length^2);
+    scatter3(psm1_pts_Polaris_cube(:,1), psm1_pts_Polaris_cube(:,2), psm1_pts_Polaris_cube(:,3), 'filled');
+
+    axis equal;
+    pm.plot;
+end
+axis equal;
+hold off;
+
+disp('plane rms:');
+rms
+
+%% Visualise the points
+
+figure('Name','Polaris Points');
+axis equal;
+scatter3(psm1_pts_Polaris_cube(:,1), psm1_pts_Polaris_cube(:,2), psm1_pts_Polaris_cube(:,3), 'filled');
+axis equal;
+hold off;
+% 
+
+
+
+
+% figure('Name', 'Distribution of plane fitting errors');
+% histfit(rms);
+% hold off;
