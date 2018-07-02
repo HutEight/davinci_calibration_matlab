72e5b9bb6b6f760140d881a431cf3802b1ec0611
HMS Prince of Wales <sxh749@case.edu>
2018-07-01 20:04:19 -0400

    rename folder

--- 20180617_new_calibration_MK10/Tool_PSM_intrinsic_tool/davinci_sphere_fit_least_square.m
+++ 20180617_new_calibration_MK10/Tool_PSM_intrinsic_tool/davinci_sphere_fit_least_square.m
@@ -3,7 +3,7 @@ function [sphere_para, residuals] = davinci_sphere_fit_least_square(pt_mat)
 
             sphere_pts = pt_mat;
 
-            pc_sphere_rover = pointCloud([sphere_pts(:+,1), sphere_pts(:,2), sphere_pts(:,3)]);
+            pc_sphere_rover = pointCloud([sphere_pts(:,1), sphere_pts(:,2), sphere_pts(:,3)]);
             
 %             points_colour_yellow_rover = uint8(zeros(pc_sphere_rover.Count, 3));
 %     % colour in r g b [0-255]
@@ -20,14 +20,14 @@ function [sphere_para, residuals] = davinci_sphere_fit_least_square(pt_mat)
 
 
 
-    [center,radius,residuals_temp]  = spherefit(sphere_pts);
+    [centre,radius,residuals_temp]  = spherefit(sphere_pts);
 % RMS
 % rms_sphere = @(a)sqrt(((transpose(pc_sphere(:,1) - a(1))*(pc_sphere(:,1) - a(1)))...
 %     + (transpose(pc_sphere(:,2) - a(2))*(pc_sphere(:,2) - a(2)))...
 %     + (transpose(pc_sphere(:,3) - a(3))*(pc_sphere(:,3) - a(3)))...
 %     - size_pc*(a(4)*a(4)))/(size_pc-1));
-center;
+centre;
 radius;
-sphere_para = [transpose(center),  radius];
+sphere_para = [transpose(centre),  radius];
 residuals = residuals_temp;
 end
